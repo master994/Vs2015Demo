437b21c47ec7d91266ee424a54f4fbd1f83dec7e
Bodrogi Krisztian <krisztian.bodrogi@possible.com>
2015-04-13 21:20:55 +0200

    Init check-in fellows:)

---
+++ src/VS2015Demo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+
+namespace VS2015Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<int> elements = new List<int>() { 10, 20, 31, 40 };
+            int oddIndex = elements.FindIndex(x => x % 2 != 0);
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
